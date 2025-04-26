724963e45751032c5fc88cd57dddb5b83db03022
artursopelnik <artur.sopelnik93@gmail.com>
2025-04-26 10:52:00 +0200

    wip

--- packages/starlight-theme-visa/index.ts
+++ packages/starlight-theme-visa/index.ts
@@ -14,6 +14,7 @@ export default function starlightThemeVisaPlugin(): StarlightPlugin {
           components: overrideComponents(config, ['LanguageSelect', 'Pagination', 'ThemeSelect'], logger),
           customCss: [
             ...(config.customCss ?? []),
+            'starlight-theme-visa/styles/fonts',
             'starlight-theme-visa/styles/layers',
             'starlight-theme-visa/styles/theme',
             'starlight-theme-visa/styles/base',
@@ -26,20 +27,20 @@ export default function starlightThemeVisaPlugin(): StarlightPlugin {
                   themes: ['vitesse-dark', 'vitesse-light'],
                   ...userExpressiveCodeConfig,
                   styleOverrides: {
-                    borderColor: 'var(--sl-rapide-ui-border-color)',
+                    borderColor: 'var(--sl-visa-ui-border-color)',
                     borderRadius: '0.5rem',
                     ...userExpressiveCodeConfig.styleOverrides,
                     frames: {
                       editorActiveTabIndicatorTopColor: 'unset',
                       editorActiveTabIndicatorBottomColor: 'var(--sl-color-gray-3)',
-                      editorTabBarBorderBottomColor: 'var(--sl-rapide-ui-border-color)',
+                      editorTabBarBorderBottomColor: 'var(--sl-visa-ui-border-color)',
                       frameBoxShadowCssValue: 'unset',
                       ...userExpressiveCodeConfig.styleOverrides?.frames,
                     },
                     textMarkers: {
                       backgroundOpacity: '40%',
-                      markBackground: 'var(--sl-rapide-ec-marker-bg-color)',
-                      markBorderColor: 'var(--sl-rapide-ec-marker-border-color)',
+                      markBackground: 'var(--sl-visa-ec-marker-bg-color)',
+                      markBorderColor: 'var(--sl-visa-ec-marker-border-color)',
                       ...userExpressiveCodeConfig.styleOverrides?.textMarkers,
                     },
                   },
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
-# [Starlight Theme Visa Monorepo](#)
+# [Starlight Theme Visa Docs Monorepo](#)
 
-...
+wip
+
+Note: I use IBM Plex Sans as an open-source alternative to Visa Dialect UI. 
+
+If you don't like the Starmind Astro default icons and your are looking for an open source version of the Visa icons, you can look at Lucide Icons:
+
+https://lucide.dev/
+
+
+Visa d
